d8a2c0b2894764cd96e22b3a87f6508c3129c139
MW-DC-Policy-Center <michael@dcpolicycenter.org>
2017-03-29 09:51:00 -0400

    New legislation folder for saved legislation as well as a new run from 3/29/17

--- newLegislationPull.py
+++ newLegislationPull.py
@@ -14,9 +14,13 @@ council period.
 '''
 #End of intro comments
 #Requests for POST call, json for parsing, pprint for pretty print outputs, pandas for database, csv for csv writing
+import time
+tic = time.time() # Setting start time of program to do a time elapsed/ tic toc print statement
 import requests, json, pprint, pandas, csv, dcLegislation, os
+
 #Building Request
 verbose = True
+tic_toc_track = True
 rowLimit = '100'                               #seems to be the max limit
 offSet = 0                                     #will change to string in the URI
 q = {"CouncilPeriod": "22"}                    #Sets advanced search query to search for current council period (22)
@@ -31,6 +35,7 @@ dataHeaders = [     'Id','CouncilPeriod','ShortTitle','Introducer',
                     'CommitteeReferralComments','Deemed','DeemedOn',
                     'LegislationStatus','Category','Subcategory','DocumentUrl']
 introducerException = 0
+if(tic_toc_track):toc_initialize = time.time()
 
 
 
@@ -52,6 +57,7 @@ with open('newLegislation.json','w') as f:
     pprint.pprint(toWrite,f)
 f.close()
 if(verbose):print('\nJSON file done...')
+if(tic_toc_track):toc_json_write = time.time()
 
 
 
@@ -63,7 +69,9 @@ with open('tmp.csv','w', newline='',encoding='utf-8') as f:
         csvHeader.append(key)
     csvWriter.writerow(csvHeader)
     for i in range(len(data_json)):
-        #if(verbose):print('\n!!! Processing Legislation number:%i of %i'%(i,len(data_json)))
+        if(verbose):
+            if(i%30 == 0):
+                print('\n!!! Processing Legislation number:%i of %i'%(i,len(data_json)))
         LN = data_json[i]['LegislationNumber']               # LN: Legislation Number
         LND = dcLegislation.apiCalls.getDetails(LN)          # LND; Legislation Number Details
         jLND = LND.json()                                    # jLND: JSON format of Legislation Number Details
@@ -80,6 +88,7 @@ with open('tmp.csv','w', newline='',encoding='utf-8') as f:
         #pprint.pprint(data_json[i]['LegislationNumber'],f)
 f.close()
 if(verbose):print('\nCSV file done...')
+if(tic_toc_track):toc_csv_write = time.time()
 
 
 
@@ -90,6 +99,23 @@ df = df.sort_values(by='Id',ascending=False)
 if(verbose):print('\nWriting dataframe to CSV file...')
 df.to_csv('New Legislation.csv', index=False, columns=dataHeaders)
 
+if(tic_toc_track):toc_pandas_write = time.time()
+
+
+
 print('Cleaning up directory...')
 os.remove('newLegislation.json')
 os.remove('tmp.csv')
+
+if tic_toc_track == True:
+    tic_toc_initialize = toc_initialize - tic
+    tic_toc_json_write = toc_json_write - tic
+    tic_toc_csv_write = toc_csv_write - tic
+    tic_toc_pandas_write = toc_pandas_write - tic
+
+    print('\n**********************\n')
+    print('Timing metrics...\n')
+    print('Tic Toc Initialize:  %s seconds...\n'%(str(tic_toc_initialize)))
+    print('Tic Toc JSON:  %s seconds...\n'%(str(tic_toc_json_write)))
+    print('Tic Toc CSV:  %s seconds...\n'%(str(tic_toc_csv_write)))
+    print('Tic Toc PANDAS:  %s seconds...\n'%(str(tic_toc_pandas_write)))
